e2b6d50a5ad594bb6f36f622533d965d2dd8eed3
codywyatt <cwyatt@pdx.edu>
2019-05-27 14:52:22 -0700

    Main file now working dir with crates added
    Added Image crate with working option to
    -Open image with command line passed value
    -Outputs to desired file format
    -Using default image crate functions for grayscale but this will be
    reworked totally
    -Started with Initial clap yml file for commands later to be implimented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate image;
+
+use image::GenericImageView;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file = &args[1];
+    
+    // Use the open function to load an image from a Path.
+    // ```open``` returns a `DynamicImage` on success.
+    let img = image::open(file).unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
+
+    // Write the contents of this image to the Writer in PNG format.
+
+    //Will Replace With custom Grayscale functions with command line interface commands
+        //Will totally rework grayscale to be custom options
+            //Things like Custom values for each color (RGB)
+            //Hopefully enough time to allow for threshold of colors to remain
+                //Picking a color of Red with color 255 out of 255 with a default percent of range to remain for example, all three colors would have this percent each time of course.
+                //Allow for that percent to be changed for all or each color
+    let gray = img.grayscale();
+    
+    //Code To Resize if needed
+        //let gray = gray.resize(gray.width()/2, gray.height()/2, image::FilterType::Nearest);
+            
+    // The color method returns the image's `ColorType`.
+    println!("Gray Color:{:?}", gray.color());
+    gray.save("out.png").unwrap();
 }
