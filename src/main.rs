1945cf8a2306ec2af08da2bc0ff52b461f9742e4
codywyatt <cwyatt@pdx.edu>
2019-05-27 21:21:25 -0700

    Added grayscale "Check"
    Added function to check the values of the grayscale to make sure it is
    how the image crate used it.
    This shows how to extract the information from the rgb pixels and turn
    them to grayscale.
    For loop with going through each pixel applying this would turn
    everything grayscale

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,29 @@ fn main() {
             //Hopefully enough time to allow for threshold of colors to remain
                 //Picking a color of Red with color 255 out of 255 with a default percent of range to remain for example, all three colors would have this percent each time of course.
                 //Allow for that percent to be changed for all or each color
+    let pixel = img.get_pixel(500,500);
+    let data = pixel.data;
+    println!("Red:{:?}  Green:{:?}  Blue:{:?}", data[0],data[1],data[2]);
+    println!("Value after grayscale below");
+    
+    //Color space defined in terms of the CIE 1931 linear luminance Y-linear
+    //(Just took the weights found online for grayscale)
+    let average = (0.2126f32 * data[0] as f32 +
+                  0.7152f32 * data[1] as f32 +
+                  0.0722f32 * data[2] as f32) as u8
+                  ;
+    println!("Red:{:?}  Green:{:?}  Blue:{:?}", average, average, average);
+    
     let gray = img.grayscale();
     
+    let pixelg = gray.get_pixel(500,500);
+    let datag = pixelg.data;
+    println!("Red:{:?}  Green:{:?}  Blue:{:?}", datag[0],datag[1],datag[2]);
+    println!("The Above two should be the exact same always");
+    
+ 
     //Code To Resize if needed
         //let gray = gray.resize(gray.width()/2, gray.height()/2, image::FilterType::Nearest);
             
-    // The color method returns the image's `ColorType`.
-    println!("Gray Color:{:?}", gray.color());
     gray.save("out.png").unwrap();
 }
